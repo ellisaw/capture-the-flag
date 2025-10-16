73ef0f94c966869978f3c9c9769ecb6dc7d64874
Ellis Asadi-Wright <e.wright097@gmail.com>
2025-10-16 19:56:47 -0400

    Implement decode logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,69 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import {sourceUrl, parentElementsData, childElementData} from './data/data.js'
 
 function App() {
+  let [decodedUrl, setDecodedUrl] = useState(''); // Destination url
+  
+  useEffect(() => {
+    // fetch source html
+    getHtmlFromUrl(sourceUrl)
+    .then(htmlString => {
+      if (htmlString) {
+        console.log("HTML content retrieved successfully:");
+        // convert html string into html
+        const parser = new DOMParser();
+        const html = parser.parseFromString(htmlString, 'text/xml');
+        decodeUrl(html)
+      } else {
+        console.log("Failed to retrieve HTML content.");
+      }
+    });
+  },[]);
+
+  const getHtmlFromUrl = async (url) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const htmlContent = await response.text();
+      return htmlContent;
+    } catch (error) {
+      console.error("Error fetching HTML:", error);
+      return null;
+    }
+  }
+
+  const decodeUrl = (html) => {
+    for (let data of parentElementsData) {
+      // node list of elements with specified tag
+      const elements = html.querySelectorAll(data.tag)
+      // regex to match attribute against
+      const regex = new RegExp(data.regex)
+      // for each element, check if the specified attribute matches the regular expression
+      elements.forEach((element) => {
+        // if attribute value doesn't match the regexp, remove it from the node list
+        if (!regex.test(element.attributes[data.attribute].value)) {
+          const parent = element.parentNode;
+          parent.removeChild(element)
+        }
+      })
+    }
+
+    let bNodes = html.querySelectorAll(childElementData.tag)
+    let str = ""
+    bNodes.forEach((node) => {
+      if(node.classList.contains(childElementData.className)) {
+        let val = node.attributes.value.nodeValue;
+        str += val
+      }
+    })
+    setDecodedUrl(str)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+export const sourceUrl = "https://tns4lpgmziiypnxxzel5ss5nyu0nftol.lambda-url.us-east-1.on.aws/challenge";
+
+export const parentElementsData = [
+  {
+    tag: "section",
+    attribute: "data-id",
+    regex: "^92"
+  },
+  {
+    tag: "section > article",
+    attribute: "data-class",
+    regex: "45$"
+  },
+  {
+    tag: "section > article > div",
+    attribute: "data-tag",
+    regex: "78"
+  },
+]
+
+export const childElementData = {
+  tag: "section > article > div > b",
+  className: "ref",
+}
